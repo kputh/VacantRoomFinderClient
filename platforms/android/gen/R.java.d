7e511599df556040210cacd48f73cb8e85c6a722
Kai <Kai.Puth@student.htw-berlin.de>
2014-01-20 17:34:21 +0100

    Default Splashscreen von Apache Cordova hinzugefügt.

--- platforms/android/gen/R.java.d
+++ platforms/android/gen/R.java.d
@@ -1,9 +1,13 @@
 C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\gen\de\htw_berlin\rz\RoomFinderApp\R.java \
  : C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable\icon.png \
-C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-hdpi\icon.png \
-C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-ldpi\icon.png \
-C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-mdpi\icon.png \
-C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-land-hdpi\screen.png \
+C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-land-ldpi\screen.png \
+C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-land-mdpi\screen.png \
+C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-land-xhdpi\screen.png \
+C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-port-hdpi\screen.png \
+C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-port-ldpi\screen.png \
+C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-port-mdpi\screen.png \
+C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\drawable-port-xhdpi\screen.png \
 C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\values\strings.xml \
 C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\res\xml\config.xml \
 C:\Users\Kai\Documents\NetBeansProjects\RoomFinderApp\platforms\android\bin\AndroidManifest.xml \
